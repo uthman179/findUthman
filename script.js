8d8309f6ca3c3d909a8481743d43fb04b9409a38
Giwa <kyzi286@gmail.com>
2023-10-30 16:53:38 +0100

    completed project

---
+++ script.js
@@ -0,0 +1,233 @@
+let toggleButton = document.getElementById("changeColorButton");
+let hero = document.querySelector(".hero");
+
+toggleButton.addEventListener("click", function() {
+  if (toggleButton.classList.contains("toggle")) {
+    toggleButton.classList.remove("toggle");
+    hero.classList.add("bg-change")
+  } else {
+    toggleButton.classList.add("toggle")
+    hero.classList.remove("bg-change")
+  }
+});
+
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('myButton');
+var closeBtn = document.getElementsByClassName('close')[0];
+
+
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+
+closeBtn.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('myButton2');
+var closeBtn = document.getElementsByClassName('close')[0];
+
+
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+
+closeBtn.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('myButton3');
+var closeBtn = document.getElementsByClassName('close')[0];
+
+
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+
+closeBtn.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('myButton4');
+var closeBtn = document.getElementsByClassName('close')[0];
+
+
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+
+closeBtn.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('myButton5');
+var closeBtn = document.getElementsByClassName('close')[0];
+
+
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+
+closeBtn.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('myButton6');
+var closeBtn = document.getElementsByClassName('close')[0];
+
+
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+
+closeBtn.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('myButton7');
+var closeBtn = document.getElementsByClassName('close')[0];
+
+
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+
+closeBtn.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('myButton8');
+var closeBtn = document.getElementsByClassName('close')[0];
+
+
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+
+closeBtn.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('myButton9');
+var closeBtn = document.getElementsByClassName('close')[0];
+
+
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+
+closeBtn.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+const buttons = document.querySelectorAll('.buttons button');
+const gridItems = document.querySelectorAll('.grid-item');
+
+let activeButton = null;
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        if (activeButton) {
+            activeButton.classList.remove('active');
+        }
+        this.classList.add('active');
+        activeButton = this;
+
+        const numToShow = parseInt(this.textContent.match(/\d+/)[0]);
+        showSquares(numToShow);
+    });
+});
+
+function showSquares(numToShow) {
+    gridItems.forEach((item, index) => {
+        if (index < numToShow) {
+            item.style.display = 'block';
+            item.style.width = '400px';
+            item.style.height = '300px';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
